32bc3073547ad5d92be4edbbd432de7dc9eafb17
Jordan Adair <43575081+JordanAdair@users.noreply.github.com>
2026-04-25 13:14:09 -0400

    Optimize repeatString.js exercise

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function (stringToRepeat, numberOfRepeats) {
+    let endString = "";
 
+    if (numberOfRepeats < 0) return "ERROR";
+
+    for (let i = 0; i < numberOfRepeats; i++) {
+        endString += stringToRepeat;
+    }
+
+    return endString;
 };
 
 // Do not edit below this line
