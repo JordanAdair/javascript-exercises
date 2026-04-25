774cd3f1b4a02b623aca6d9b143693cb29de57f8
Jordan Adair <43575081+JordanAdair@users.noreply.github.com>
2026-04-25 13:32:44 -0400

    Optimize reverseString.js exercise

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(stringToReverse) {
+  let reversedString = '';
+  for (let i = 0; i < stringToReverse.length; i++) {
+    reversedString += stringToReverse[stringToReverse.length - 1 - i];
+  }
+  return reversedString;
 };
 
 // Do not edit below this line
